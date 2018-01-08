12251dae6faeb3e40c71b8a88d4d847d0261c7a5
Eduard A <xsynergy510x@gmail.com>
2018-01-08 16:11:30 +0000

    Initial Push

---
+++ help_docs.py
@@ -0,0 +1,61 @@
+help_default='''```asciidoc
+Official Squid Squad Bot
+Help Commands:
+!help
+	- Displays this message
+!help auto
+	- Lists all automatic tasks 
+!help commands|cmds
+	- Lists all Bot commands
+!help music
+	- Lists all music commands
+```'''
+
+help_auto='''```asciidoc
+Bot listens for these words in all messages:
+bet
+	- B.E.T
+brb
+	- Reacts with :JesseBRB:
+Monika|monika
+	- Type the name right, idiot
+taco
+	- Reacts with taco
+taco bravo
+	- Gives urgent PSA warning
+ww@
+	- Try asking again
+
+Bot listens for game names/phrases and reacts with proper emote(s):
+aram
+destiny
+league
+overwatch
+pubg
+raid 
+```'''
+
+help_commands='''```asciidoc
+!8ball
+	- Ask the Magic 8-ball anything
+!carjesse WIP
+	- Use it when Car Jesse appears
+!conch
+	- All Hail the Magic Conch!
+!crypto X
+	- Ride the crypto coaster (Powered by coinmarketcap)
+!emojiparty
+	- Reacts with up to 20 random emotes
+!lootbox
+	- Open a Destiny 2 Legendary Engram. Roll some weapon
+!qotd|quote
+	-Displays Quote of the Day
+!timer
+	- Say "timer X" for an X minute timer up to 60m
+!yesno
+	- Yes or No
+```'''
+
+help_music='''```asciidoc
+WIP - ETA 2019
+```'''
---
+++ lists.py
@@ -0,0 +1,40 @@
+from IDs import IDs
+
+magic_8ball_items = {
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes definitely",
+	"You may rely on it",
+	"As I see it, yes",
+	"Most Likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+	"No"
+}
+
+yesno_items = {
+	"Decision: Yes": 48,
+	"Decision: No": 48,
+	"Try again later": 2
+}
+
+conch_items = {
+	"Maybe someday": 5,
+	"Nothing": 5,
+	"Neither": 5,
+	"I don't think so": 5,
+	"No": 7,
+	"Try asking again": 3,
+	"Yes": 1
+}
