27fa0d70c15ee6c37e79c04cc265ac5611432d56
Eduard A <xsynergy510x@gmail.com>
2018-01-09 18:30:58 +0000

    QOTD: Set absolute path for qod.json file

--- qod.py
+++ qod.py
@@ -1,7 +1,7 @@
 import urllib.request
 import json
 
-qod_filename = 'qod.json'
+qod_filename = '/home/pi/botv3/qod.json'
 url = 'http://quotes.rest/qod.json'
 response = urllib.request.urlopen(url)
 data = json.load(response)
