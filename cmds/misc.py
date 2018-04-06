2cb2f09279b5a203fdc958c4bd640d677b65bf94
Eduard A <xsynergy510x@gmail.com>
2018-04-05 19:23:11 -0700

    misc.py: Move urls array into !salt function

--- cmds/misc.py
+++ cmds/misc.py
@@ -3,14 +3,13 @@ import discord
 import random
 from discord.ext import commands
 
-urls = [
-'https://www.youtube.com/watch?v=3KquFZYi6L0'
-]
-
 class Misc:
 	def __init__(self, bot):
 		self.bot = bot
 	
 	@commands.command(pass_context=True, no_pm=True)
 	async def salt(self, ctx):
+		urls = [
+			'https://www.youtube.com/watch?v=3KquFZYi6L0'
+		]
 		await self.bot.say('{0}'.format(random.choice(urls)))	
