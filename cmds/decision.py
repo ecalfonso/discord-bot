000532fcdfdc8be631dc5b88fd826850a4d79960
Eduard A <xsynergy510x@gmail.com>
2018-05-12 00:12:12 -0700

    Add !shuffle
    Input:
    choice1 choice2 ... choiceX
    or
    Number choice1 ... choiceX
    
    Bot will choose 1 or specified amount from list given

--- cmds/decision.py
+++ cmds/decision.py
@@ -21,5 +21,19 @@ class Decision:
 		await self.bot.say('Magic 8-ball says: {0}'.format(random.sample(magic_8ball_items, 1)[0]))
 
 	@commands.command(pass_context=True, no_pm=True)
+	async def shuffle(self, ctx, *, args: str):
+		num = 1
+		l = args.split()
+		if args.split()[0].isdigit():
+			num = l[0]
+			if int(num) > len(l):
+				await self.bot.say('Choice must be below {}'.format(len(l)-1))
+			l.pop(0)
+		msg = 'Here are your {} choices: '.format(num)
+		for x in random.sample(l, int(num)):
+			msg += ' {}'.format(x)
+		await self.bot.say(msg)
+
+	@commands.command(pass_context=True, no_pm=True)
 	async def yesno(self, ctx):
 		await self.bot.say(random.choice([k for k in yesno_items for dummy in range(yesno_items[k])]))
