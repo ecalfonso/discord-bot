78c61d011ce5eb34755b1610e16942ed7fc658b7
Eduard <xsynergy510x@gmail.com>
2019-01-25 10:21:55 -0800

    Fix global_timer logic

--- global_timer/global_timer.py
+++ global_timer/global_timer.py
@@ -19,15 +19,15 @@ async def global_timer(bot):
         # Monday Check
         if now.weekday() == 0 and \
                 now.hour == 0 and \
-                now.minute == 0 or \
-                now.minute == 1:
+                (now.minute == 0 or \
+                now.minute == 1):
             await monday_punday(bot)
 
         # Wednesday check
         if now.weekday() == 2 and \
                 now.hour == 0 and \
-                now.minute == 0 or \
-                now.minute == 1:
+                (now.minute == 0 or \
+                now.minute == 1):
             await wednesday_check(bot)
 
         # 2 Minute delay between polling
